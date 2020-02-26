154a36a76663033cbd3d1d2b72f3b4c2ddf1829c
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2020-02-26 21:17:04 +0700

    gui update

--- imageviewer.py
+++ imageviewer.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from tkinter import *
+import os
 
 root = tk.Tk()
 
 root.geometry('800x730+0+0')
 root.title('Photo Viewer')
+root.config(bg = 'gray10')
 
 
 class ImageViewer(Frame):
@@ -12,28 +14,44 @@ class ImageViewer(Frame):
         Frame.__init__(self,master)
         self.master = master
 
-        self.ViewerFrame = Frame(self.master, bd = 0, bg = 'gray10', height = 670)
+        self.Folderchoose = Frame(self.master, bg = 'gray25', height = 50, width = 400)
+        self.Folderchoose.pack(side = TOP, fill = X)
+
+        self.foldr = Button(self.Folderchoose, text = ' 🗀', font = ('impact', 20), fg = 'white', bg = 'gray25',
+                            activeforeground = 'black', activebackground = 'gray', borderwidth = 0, anchor = W)
+        self.foldr.pack(side = LEFT, fill = X, expand = True)
+
+        self.FileName = Frame(self.Folderchoose, bg = 'gray15', height = 50, width = 1000)
+        self.FileName.place(x = 400, y = 0)
+
+        self.ViewerFrame = Frame(self.master, bd = 0, bg = 'gray5', height = 700)
         self.ViewerFrame.pack(side = TOP, fill = BOTH, expand = True)
 
-        self.NextButton = Button(self.master, bd = 0, text = '>', font = ('impact',25),activeforeground = 'black',fg = 'white',bg = 'gray15')
+        self.Thumbnails = Frame(self.ViewerFrame, bd = 0, bg = 'gray10')
+        self.Thumbnails.pack(side = TOP, fill = BOTH, expand = True)
+
+        self.ThumbSB = Scrollbar(self.Thumbnails, orient = VERTICAL)
+        self.ThumbSB.pack(side = RIGHT, fill = Y)
+
+        self.NextButton = Button(self.master, bd = 0, text = '>', font = ('impact',25),activeforeground = 'black',
+                                 fg = 'gray',bg = 'gray15', activebackground = 'gray20')
         self.NextButton.pack(side = RIGHT, expand = True, fill = X)
-        #self.NextButton.bind('<Button-1>', self.nextButton)
 
-        self.Zoom = Button(self.master, bd = 0, text = '+', font = ('impact',25), activeforeground = 'black',fg = 'white',bg = 'gray15')
+        self.Zoom = Button(self.master, bd = 0, text = '+', font = ('impact',25), activeforeground = 'black',
+                           fg = 'gray',bg = 'gray15', activebackground = 'gray20')
         self.Zoom.pack(side = RIGHT, expand = True, fill = X)
-        #self.Zoom.bind('<Button-1>', self.zoomButton)
 
-        self.PrevButton = Button(self.master, bd = 0, text = '<', font = ('impact',25), activeforeground = 'black',fg = 'white', bg = 'gray15')
+        self.PrevButton = Button(self.master, bd = 0, text = '<', font = ('impact',25), activeforeground = 'black',
+                                 fg = 'gray', bg = 'gray15', activebackground = 'gray20')
         self.PrevButton.pack(side = RIGHT, expand = True, fill = X)
-        #self.PrevButton.bind('<Button-1>',self.prevButton)
 
-    def nextButton(self,evt):
+    def nextButton(self):
         pass
 
-    def zoomButton(self,evt):
+    def zoomButton(self):
         pass
 
-    def prevButton(self,evt):
+    def prevButton(self):
         pass
 
 app = ImageViewer(root).pack()
