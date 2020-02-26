ce8051c6f296dbbe18be58b5fe94ed72833f4730
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2020-02-26 23:43:17 +0700

    update gui

--- imageviewer.py
+++ imageviewer.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
 import os
 
 root = tk.Tk()
@@ -8,11 +10,16 @@ root.geometry('800x730+0+0')
 root.title('Photo Viewer')
 root.config(bg = 'gray10')
 
-
 class ImageViewer(Frame):
     def __init__(self, master):
         Frame.__init__(self,master)
         self.master = master
+        self.filetypes = ('.jpg','.jpeg','.png','.gif')
+        self.images = list()
+
+        self.ROW = 0
+        self.COL = 0
+        self.BUTTONCOUNT = 0
 
         self.ViewerFrame = Frame(self.master, bd = 0, bg = 'gray5', height = 700)
         self.ViewerFrame.pack(side = TOP, fill = BOTH, expand = True)
@@ -20,30 +27,28 @@ class ImageViewer(Frame):
         self.Thumbnails = Frame(self.ViewerFrame, bd = 0, bg = 'gray10')
         self.Thumbnails.pack(side = TOP, fill = BOTH, expand = True)
 
-        self.ThumbSB = Scrollbar(self.Thumbnails, orient = VERTICAL)
-        self.ThumbSB.pack(side = RIGHT, fill = Y)
-
         self.Folderchoose = Frame(self.master, bg = 'gray25', height = 50, width = 400)
         self.Folderchoose.pack(side = TOP, fill = X)
 
-        self.foldr = Button(self.Folderchoose, text = ' 🗀', font = ('impact', 20), fg = 'white', bg = 'gray25',
-                            activeforeground = 'black', activebackground = 'gray', borderwidth = 0, anchor = W)
+        self.foldr = Button(self.Folderchoose, text = '                    🗀', font = ('calibri', 20), fg = 'white', 
+                            bg = 'gray25',activeforeground = 'black', activebackground = 'gray40', borderwidth = 0, anchor = W,
+                            command = self.FolderOpen)
         self.foldr.pack(side = LEFT, fill = X, expand = True)
 
-        self.FileName = Frame(self.Folderchoose, bg = 'gray15', height = 50, width = 1000)
-        self.FileName.place(x = 400, y = 0)
+        self.FileName = Frame(self.Folderchoose,bd = 0, bg = 'gray5', height = 50, width = 1120)
+        self.FileName.place(x = 280, y = 5)
 
         self.NextButton = Button(self.master, bd = 0, text = '>', font = ('impact',18),activeforeground = 'white',
                                  fg = 'gray',bg = 'gray15', activebackground = 'gray15')
         self.NextButton.pack(side = RIGHT, expand = True, fill = X)
 
-        self.Zoom = Button(self.master, bd = 0, text = '+', font = ('impact',18), activeforeground = 'white',
-                           fg = 'gray',bg = 'gray15', activebackground = 'gray15')
-        self.Zoom.pack(side = RIGHT, expand = True, fill = X)
 
         self.PrevButton = Button(self.master, bd = 0, text = '<', font = ('impact',18), activeforeground = 'white',
                                  fg = 'gray', bg = 'gray15', activebackground = 'gray15')
         self.PrevButton.pack(side = RIGHT, expand = True, fill = X)
+
+    def FolderOpen(self):
+        self.FolderOP = filedialog.askdirectory()
 
     def nextButton(self):
         pass
