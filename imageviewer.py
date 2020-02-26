a0b63d7ebe59fe1d6a1e15f29d6eb89d28e0628d
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2020-02-27 01:15:24 +0700

    updatey

--- imageviewer.py
+++ imageviewer.py
@@ -16,6 +16,8 @@ class ImageViewer(Frame):
         self.master = master
         self.filetypes = ('.jpg','.jpeg','.png','.gif')
         self.images = list()
+        self.w = 0
+        self.h = 0
 
         self.ViewerFrame = Frame(self.master, bd = 0, bg = 'gray5', height = 700)
         self.ViewerFrame.pack(side = TOP, fill = BOTH, expand = True)
@@ -45,6 +47,7 @@ class ImageViewer(Frame):
         self.PrevButton.pack(side = RIGHT, expand = True, fill = X)
 
     def FolderOpen(self):
+        self.resolution = None
         self.FolderOP = filedialog.askdirectory()
 
         for i in os.listdir(self.FolderOP):
@@ -52,16 +55,24 @@ class ImageViewer(Frame):
                 self.images.append(i)
 
         self.firstimg = ("{0}/{1}".format(self.FolderOP, self.images[0]))
-        print(self.firstimg)
         self.img = Image.open(self.firstimg)
-        self.img = self.img.resize((900,640), Image.ANTIALIAS)
+        self.w,self.h = self.img.size
+
+        if self.w == self.h:
+            self.resolution = (640,640)
+        elif self.w > self.h:
+            self.resolution = (1150,640)
+        elif self.w < self.h:
+            self.resolution = (440,640)
+            
+        self.img = self.img.resize(self.resolution, Image.ANTIALIAS)
 
         self.img = ImageTk.PhotoImage(self.img)
 
-        self.panel = Label(self.Imageviewer, image = self.img, bg = 'gray') 
+        self.imgview = Label(self.Imageviewer, image = self.img, bg = 'gray') 
       
-        self.panel.image = self.img 
-        self.panel.pack(side = TOP, fill = BOTH, expand = True)
+        self.imgview.image = self.img 
+        self.imgview.pack(side = TOP, fill = BOTH, expand = True)
 
         self.FilenameLabel.config(text = '     File Name: {0}'.format(self.images[0]))        
 
